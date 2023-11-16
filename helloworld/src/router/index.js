a4094370d2159b989da8b8515fe5cb0a23fbed02
LamaChihabi <107999296+LamaChihabi@users.noreply.github.com>
2023-11-16 18:00:45 +0100

    vue.js "Hello World "erstellt

---
+++ helloworld/src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '@/views/About.vue'
+import Fragen from '@/views/Fragen.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/Fragen',
+    name: 'Fragen',
+    component: Fragen
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  linkActiveClass: 'active',
+  routes
+})
+
+export default router

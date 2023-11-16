51d67d670f7d258436020b30383f362145347930
LamaChihabi <107999296+LamaChihabi@users.noreply.github.com>
2023-11-16 02:21:35 +0100

    vue.js "Hello World "erstellt

---
+++ helloworld/src/views/Fragen.vue
@@ -0,0 +1,15 @@
+<template>
+<h1>Welcome to Fragen </h1>
+</template>
+
+<script >
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Fragen'
+}
+
+</script>
+
+<style scoped>
+
+</style>
